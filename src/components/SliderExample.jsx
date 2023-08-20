2dbcb5aa1435fd0a2a399429ef74d02b8bb9beb9
Tinu Ade <tinucy@yahoo.com>
2023-08-20 17:54:49 +0100

    added even handler to slider and the value of its default useState to zero

--- src/components/SliderExample.jsx
+++ src/components/SliderExample.jsx
@@ -1,12 +1,18 @@
 import React, { useState } from "react";
 
 export default function SliderExample() {
-  const [SliderValue, setSliderValue] = useState("70");
+  const [SliderValue, setSliderValue] = useState("0");
 
   return (
     <div>
       <h2>SliderExample</h2>
-      <input type="range" min="0" max="100" value={SliderValue} />
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={SliderValue}
+        onChange={(e) => setSliderValue(e.target.SliderValue)}
+      />
       <p>Slider Value: {SliderValue}</p>
     </div>
   );
